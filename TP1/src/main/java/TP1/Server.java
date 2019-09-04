5740b522691fafc7a305df90b039d2e55d611c83
Facundo <facundogiannotti@gmail.com>
2019-09-03 20:25:22 -0400

    create server

--- TP1/src/main/java/TP1/Server.java
+++ TP1/src/main/java/TP1/Server.java
@@ -9,10 +9,6 @@ import java.net.Socket;
 import java.util.List;
 import java.util.Stack;
 
-/** Le client doit �tre capable de lire un fichier texte et d�envoyer son contenu au serveur qui retransmettra aussit�t son contenu au client. Ce dernier devra intercepter le contenu du fichier texte. Une fois la r�ception termin�e, le serveur devra inverser le contenu du fichier de sorte � ce que la premi�re ligne re�ue soit la derni�re ligne envoy�e vers le client. **/
-//L'impl�mentation du serveur n'est pas multi-threaded. Ainsi, la connection de
-//plusieurs clients en m�me temps au serveur ne fonctionnera pas! A vous de threader le serveur
-//pour qu'il puisse avoir la capacit� d'accepter plusieurs clients.
 public class Server {
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		while (true) {
@@ -21,30 +17,31 @@ public class Server {
 			ObjectInputStream in = null;
 			ObjectOutputStream out = null;
 			try {
-				// Cr�ation du socket du serveur en utilisant le port 5000.
-				serverSocket = new ServerSocket(5000);
-				// Ici, la fonction accept est bloquante! Ainsi, l'ex�cution du serveur s'arr�te
+				// Création du socket du serveur en utilisant le port 5000.
+				serverSocket = new ServerSocket(5003);
+				// Ici, la fonction accept est bloquante! Ainsi, l'exécution du serveur s'arrête
 				// ici et attend la connection d'un client avant de poursuivre.
 				socket = serverSocket.accept();
-				// Cr�ation d'un input stream. Ce stream contiendra les donn�es envoy�es par le
+				// Création d'un input stream. Ce stream contiendra les données envoyées par le
 				// client.
 				in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
-				// La fonction readObject est bloquante! Ainsi, le serveur arr�te son ex�cution
-				// et attend la r�ception de l'objet envoy� par le client!
+				// La fonction readObject est bloquante! Ainsi, le serveur arrête son exécution
+				// et attend la réception de l'objet envoyé par le client!
 				List<String> strings = (List<String>) in.readObject();
 				Stack<String> stackOfLines = new Stack<String>();
-				// Remplissage de la stack avec les lignes. La premi�re ligne entr�e sera la
-				// derni�re � ressortir.
+				// Remplissage de la stack avec les lignes. La première ligne entrée sera la
+				// dernière à ressortir.
 				for (int i = 0; i < strings.size(); i++) {
 					stackOfLines.push(strings.get(i));
 				}
-				// Cr�ation du output stream. Ce stream contiendra les donn�es qui seront
-				// envoy�es au client.
+				// Création du output stream. Ce stream contiendra les données qui seront
+				// envoyées au client.
 				out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-				// �criture des donn�es dans la pile.
+				// Écriture des données dans la pile.
 				out.writeObject(stackOfLines);
-				// Envoi des donn�es vers le client.
+				// Envoi des données vers le client.
 				out.flush();
+
 			} finally {
 				serverSocket.close();
 				socket.close();
