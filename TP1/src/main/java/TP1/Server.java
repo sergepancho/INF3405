9040a78e49e25f74cd9191aa7c7cc066e21eb554
Facundo <facundogiannotti@gmail.com>
2019-10-20 16:53:38 -0400

    [wip] ajout de mkdir

--- TP1/src/main/java/TP1/Server.java
+++ TP1/src/main/java/TP1/Server.java
@@ -10,6 +10,7 @@ import java.net.ServerSocket;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 
 public class Server {
@@ -30,7 +31,7 @@ public class Server {
 
 	public static void main(String[] args) throws Exception {
 		int clientNumber = 0;
-		String serverAddress = "132.207.29.123";
+		String serverAddress = "132.207.29.122";
 		int serverPort = 5003;
 
 		// creation de la connexion
@@ -47,7 +48,8 @@ public class Server {
 			while (true) {
 				new ClientHandler(listener.accept(), clientNumber++).start();
 			}
-		} finally {
+		} 
+		finally {
 			listener.close();
 		}
 	}
@@ -70,46 +72,57 @@ public class Server {
 				DataInputStream in = new DataInputStream(socket.getInputStream());
 				String commandName;
 				do{	
-				String clientCommand = in.readUTF();
-				String command[] = clientCommand.split("\\ ");//le premier string est le nom de la commande 
-				commandName = command[0];
-		
-				System.out.println("rien");
-				System.out.println(commandName);
+					String clientCommand = in.readUTF();
+					//System.out.println(clientCommand);
+					String command[] = clientCommand.split(" ");//le premier string est le nom de la commande 
+					commandName = command[0];
+			
+				//	System.out.println("rien");
+					//System.out.println(commandName);
+					
+						System.out.println("valeur de la commande"+Arrays.toString(command));
+					
 				
-					switch (commandName) {
-					case "ls":
-						File dir = new File(System.getProperty("user.dir"));
+							switch (commandName) {
+								case "ls":
+									File dir = new File(System.getProperty("user.dir"));
 
-						String[] fileNames = dir.list();
-						String response = "";
-						for (String fileName : fileNames) {
-							response += fileName + ";";
-							System.out.println(fileName + "  ");
-						}
-						out.writeUTF(response);
-						break;
+									String[] fileNames = dir.list();
+									String response = "";
+									for (String fileName : fileNames) {
+										response += fileName + ";";
+										System.out.println(fileName + "  ");
+									}
+									out.writeUTF(response);
+									break;
 
-					case "cd":
-						break;
+								case "cd":
+									break;
 
-					case "mkdir":
-						break;
-	
-					case "upload":
-						break;
-	
-					case "download":
-						break;
-	
-					case "exit":
-						break;
-	
-					default:
-						break;
-				}
+								case "mkdir":
+								String name =command[1];
+								File directory = new File(name);
+								if(!directory.exists()){ // verifier que le dossier  a ete cree 
+									if(directory.mkdir()){
+										out.writeUTF("Le fichier "+name+"a ;ete creer"); 
+									}
+								}
+									break;
+				
+								case "upload":
+									break;
+				
+								case "download":
+									break;
+				
+								case "exit":
+									break;
+				
+								default:
+									break;
+							}
 
-				System.out.println("Command from client " + commandName);
+						System.out.println("Command from client " + commandName);
 
 				}while(commandName != "exit");
 
@@ -127,24 +140,24 @@ public class Server {
 		}
 
 
-	private static  uploadFile(String s) {
-        try {
-            Path file = currentDir.resolve(s);
-            if (file.toFile().exists() && file.toFile().isFile()) {
-                FileInputStream fileStream = new FileInputStream(file.toFile());
-                long size = file.toFile().length();
-                socket.getOut().writeLong(size);
-                copyStreamUpload(fileStream, socket.getOut());
-                fileStream.close();
-            } else {
-                socket.getOut().writeLong(0);
-                System.out.println("No such file was found!");
-            }
-        } catch (InvalidPathException e) {
-            System.out.println("The file doesn't exist");
+		/*private static  uploadFile(String s) {
+			try {
+				Path file = currentDir.resolve(s);
+				if (file.toFile().exists() && file.toFile().isFile()) {
+					FileInputStream fileStream = new FileInputStream(file.toFile());
+					long size = file.toFile().length();
+					socket.getOut().writeLong(size);
+					copyStreamUpload(fileStream, socket.getOut());
+					fileStream.close();
+				} else {
+					socket.getOut().writeLong(0);
+					System.out.println("No such file was found!");
+				}
+			} catch (InvalidPathException e) {
+				System.out.println("The file doesn't exist");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-	}	}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}*/
+	}	//}
 }
